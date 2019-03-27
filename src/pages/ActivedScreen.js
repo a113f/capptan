e74354a7aa64af7d423009db1528f7cc4e637868
Allef Gomes <allefdev@gmail.com>
2019-03-26 22:29:35 -0300

    Create pages of project

---
+++ src/pages/ActivedScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class ActivedScreen extends Component {
+  render() {
+    return <Text>ActivedScreen</Text>;
+  }
+}
+
+export default ActivedScreen;
---
+++ src/pages/SignInScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class SignInScreen extends Component {
+  render() {
+    return <Text>SignInScreen</Text>;
+  }
+}
+
+export default SignInScreen;
