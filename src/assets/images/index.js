e1ad8ab29b075a5dd6b7f28b5a2134be4b02cc20
Allef Gomes <allefdev@gmail.com>
2019-03-26 22:28:50 -0300

    Add images of project

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+const LOGO = require('./logo.png');
+const LOGO_CAPPTAN = require('./logo_cappitan.png');
+const MOBILE_EXPERT = require('./mobile_expert.png');
+const VERTICAL_MOBILE_EXPERT = require('./vertical_mobile_expert.png');
+
+export { LOGO, LOGO_CAPPTAN, MOBILE_EXPERT, VERTICAL_MOBILE_EXPERT }
