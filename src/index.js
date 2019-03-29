972511128de7d2e506abf36b952301f329fe49da
Allef Gomes <allefdev@gmail.com>
2019-03-28 23:28:22 -0300

    Add redux and create actions of user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Routes from './routes/';
+import storeConfig from './store/';
+import { Provider } from 'react-redux';
 
-const App = () => <Routes />
+const store = storeConfig();
+
+const App = () =>
+  <Provider store={store}>
+    <Routes />
+  </Provider>
 
 export default App;
